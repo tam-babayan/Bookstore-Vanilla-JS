ae65350d5f5b2d248d13721d75039358385e8b39
tam-babayan <tam.babayan@gmail.com>
2018-12-18 15:43:55 +0100

    search filer

--- bookstore.js
+++ bookstore.js
@@ -6,15 +6,16 @@ function getData() {
     fetch('https://api.myjson.com/bins/udbm5')
         .then(response => response.json())
         .then(data => {
-            fillData(data)
+            fillData(data.books)
+            books = data.books
             console.log(data)
         })
         .catch(error => console.error(error))
 }
 
-function fillData(data) {
+function fillData(x) {
     let container = document.querySelector('#container')
-    for (let i = 0; i < data.books.length; i++) {
+    for (let i = 0; i < x.length; i++) {
 
         let flipCard = document.createElement('div')
         flipCard.className = "flip-card"
@@ -28,24 +29,24 @@ function fillData(data) {
         cardFront.className = "flip-card-front"
         let image = document.createElement('IMG')
         image = new Image(350, 450)
-        image.src = data.books[i].portada
+        image.src = x[i].portada
         cardFront.append(image)
         flipCardInner.append(cardFront)
 
         let cardBack = document.createElement('div')
         cardBack.className = "flip-card-back"
         let titel = document.createElement('h3')
-        titel.innerHTML = data.books[i].titulo
+        titel.innerHTML = x[i].titulo
         cardBack.append(titel)
         let description = document.createElement('p')
-        description.innerHTML = data.books[i].descripcion
+        description.innerHTML = x[i].descripcion
         cardBack.append(description)
         flipCardInner.append(cardBack)
 
         let btn = document.createElement("BUTTON")
         btn.setAttribute("data-toggle", "modal")
         btn.setAttribute("data-target", "#exampleModalCenter" + i)
-        btn.setAttribute("type","button")
+        btn.setAttribute("type", "button")
         btn.className = "btn btn-warning"
         let text = document.createTextNode("See More")
         btn.appendChild(text)
@@ -57,19 +58,24 @@ function fillData(data) {
         modal.setAttribute("tabindex", "-1")
         modal.setAttribute("role", "dialog")
         modal.setAttribute("aria-labelledby", "exampleModalCenterTitle")
-        modal.setAttribute("aria-hidden","true")
+        modal.setAttribute("aria-hidden", "true")
         container.appendChild(modal)
 
         let modalDialog = document.createElement('div')
         modalDialog.className = "modal-dialog modal-dialog-centered"
         modalDialog.setAttribute("role", "document")
         modal.appendChild(modalDialog)
-        
-        let modalContent = document.createElement('IMG')
-        modalContent.className = "modal-content"
-        modalContent.src = data.books[i].detalle
-        modalDialog.appendChild(modalContent)
 
+        let modalContent = document.createElement('IMG')
+        modalContent.src = x[i].detalle
+        modalDialog.appendChild(modalContent)
     }
 }
 
+function searchFunction() {
+    document.querySelector('#container').innerHTML = ''
+    var searchValue = document.getElementById("book-name").value
+    var filteredobject = books.filter(book => book.titulo.includes(searchValue) || book.titulo.toLowerCase().includes(searchValue))
+    console.log(filteredobject)
+    var result = (filteredobject.length) ? fillData(filteredobject) : (document.querySelector('#container').innerHTML = "No matching results")
+}
